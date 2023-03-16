c1a1f01d533809fe18630bc7104bd0174922945e
Sharona Young <seyitdesignsllc@gmail.com>
2023-03-16 18:24:29 +0100

    updated js

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
 const mainEl = document.querySelector('main');
 const submit = document.querySelector('#submit');
 const nameInput = document.querySelector('#name');
- 
-submit.addEventListener('click', function(event) {
-    event.preventDefault();
-    console.log("Button pressed");
-    
+const button = document.querySelectorAll('button');
 
+const names = [];
+ 
+mainEl.addEventListener('click', function(event) {
+    event.preventDefault();
+    if(event.target.matches("#submit")) {
+       console.log("button");
+       
     // add button when submit is clicked
     const nameButton = document.createElement("button");
     nameButton.textContent = nameInput.value
-    nameInput.value = "";
+    
+    if(names.includes(nameInput.value)) {
+        console.log("repeat");
+        
+        // add button next to existing button with the same name     
+    }
+    
     mainEl.append(nameButton);
+
+    names.push(nameInput.value);
+    console.log(names);
+    nameInput.value = "";
+    }
 });
+    
+
+    
