fb366f53ab15815fd558d8828ae075d10b89c1a9
Sharona Young <msrona806@gmail.com>
2023-03-21 10:44:43 -0400

    deleted unused variables

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 const mainEl = document.querySelector('main');
-const submit = document.querySelector('#submit');
 const nameInput = document.querySelector('#name');
-const button = document.querySelectorAll('button');
 
 // array to hold names
 const names = [];
@@ -9,7 +7,7 @@ const names = [];
 mainEl.addEventListener('click', function(event) {
     event.preventDefault();
     if(event.target.matches("#enterName")) {
-           
+    
     // add button when submit is clicked
     const nameButton = document.createElement("button");
     // place name entered on button
