be319b5c69ca8e746d0f8071d84b71d651d6c337
Sharona Young <msrona806@gmail.com>
2023-03-26 18:18:48 -0400

    reworked functions

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
-const mainEl = document.querySelector('main');
+const form = document.querySelector('form');
 const nameInput = document.querySelector('#name');
+const namesEl = document.querySelector('#nameBtns');
 
-// array to hold names
-const names = [];
- 
-mainEl.addEventListener('click', function(event) {
+// function for submit button 
+form.addEventListener('submit', function(event){
     event.preventDefault();
-    if(event.target.matches("#enterName")) {
-    
+  
     // add button when submit is clicked
     const nameButton = document.createElement("button");
     // place name entered on button
-    nameButton.textContent = nameInput.value        
-    mainEl.append(nameButton);
-    // add name to array
-    names.push(nameInput.value);
+    nameButton.textContent = nameInput.value
+    nameButton.classList.add("name");     
+    namesEl.append(nameButton);
+    // clear input box
     nameInput.value = "";
+})
+
+// function for each button
+namesEl.addEventListener('click', function(event) {
+    // event.preventDefault();
+    if(event.target.matches(".name")) {
+        console.log(event.target.innerText);
     }
 });
